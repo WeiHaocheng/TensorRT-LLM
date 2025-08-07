3d01107e8b0d8171ff51cc27e7d5f95e5a01084c
Fred Wei <20514172+WeiHaocheng@users.noreply.github.com>
2025-08-06 21:44:58 -0700

    draft: fuse moe prepare counter and expert static

--- cpp/tensorrt_llm/kernels/moePrepareKernels.h
+++ cpp/tensorrt_llm/kernels/moePrepareKernels.h
@@ -61,11 +61,13 @@ static constexpr int FIFO_SIZE_IN_U64 = PACKET_SIZE_IN_U64 * PACKET_PER_STEP * S
 #define ALIGN_256 alignas(256)
 #endif
 
+#define VALUE_SIZE 512
+
 struct ALIGN_256 MoeCommFifoConnInfo
 {
     volatile uint64_t head;  // write position
     volatile uint64_t tail;  // read position
-    volatile uint64_t count; // for counter
+    volatile int values[VALUE_SIZE]; // for values
 };
 
 struct MoeCommWorkspace
@@ -104,7 +106,7 @@ struct MoeCommWorkspace
 };
 
 void computeCountAndIndice(int* experts, int* sendCounts, int* recvCounts, int* sendIndiceWorkspace,
-    int* backwardIndiceWorkspace, int* recvIndiceWorkspace, MoeCommWorkspace workspace, int tokenCount,
+    int* backwardIndiceWorkspace, int* recvIndiceWorkspace, int* expertStatics, int* gatheredExpertStatics, MoeCommWorkspace workspace, int tokenCount,
     int maxTokenCountPerRank, int topK, int expert_count, int rankId, int rankCount, cudaStream_t stream);
 
 void computeCumsum(int* sendCountsCumsum, int* recvCountsCumsum, int rankId, int rankCount, cudaStream_t stream);
